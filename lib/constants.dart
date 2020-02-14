cb9d86173f394257a90f68dc1c9c270163ebf3b5
Wael Zantour <zantour.1999@gmail.com>
2020-02-14 16:31:30 +0100

    chore: creat MessageTextFieldDecoration constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,9 @@ const kSendButtonTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
   fontSize: 18.0,
 );
+
+const kMessageTextFieldDecoration = InputDecoration(
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  hintText: 'Type your message here...',
+  border: InputBorder.none,
+);
